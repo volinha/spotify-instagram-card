14a4099141e6a59d35a70859dc36ef3b3229923a
Vinícius Fernandes <viniciusfernandesmct@gmail.com>
2022-01-23 08:27:51 -0300

    updated: (alot of changes) lyricscard

--- src/components/LyricsCard/LyricsCard.jsx
+++ src/components/LyricsCard/LyricsCard.jsx
@@ -1,8 +1,11 @@
-import React from "react";
-import styled, { keyframes } from "styled-components";
+import React, { useState } from "react";
+import styled from "styled-components";
 
-import cover from "../../img/supercombo.png";
-import { ReactComponent as Spotify } from "../../img/SpotWhite.svg";
+import { ReactComponent as Spotify } from "../../img/spotify-seeklogo.com.svg";
+
+import Amianto from "../../img/supercombo-amianto.jpg";
+import CafeAmargo from "../../img/supercombo-cafeamargo.png";
+import TodoCarinho from "../../img/dudabeat-todocarinho.jpg";
 
 const cardSize = 300; //px
 
@@ -13,20 +16,33 @@ const lyricsBoxSizeToContent = 0.5957446808510638;
 const logoBoxSizeToContent = 0.0851063829787234;
 
 const Wrapper = styled.div`
-margin: 0;
-padding: 0;
+  margin: 0;
+  padding: 0;
   width: 100%;
   height: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-color: ${(props) => props.primaryColor};
+  background-color: ${(props) => props.color};
+`;
+
+const ButtonWrapper = styled.div`
+  position: absolute;
+  bottom: 0;
+  height: 50px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.color};
 `;
 
 const Card = styled.div`
   width: ${cardSize - 2 * (cardSize * paddingToCard) + "px"};
+  min-width: ${cardSize};
   height: ${cardSize - 2 * (cardSize * paddingToCard) + "px"};
-  background: ${(props) => props.primaryColor};
+  background: ${(props) => props.color};
   border-radius: 5px;
   display: flex;
   justify-content: center;
@@ -34,7 +50,14 @@ const Card = styled.div`
   flex-direction: column;
   padding: ${cardSize * paddingToCard + "px"};
 
-  box-shadow: 0px 0px 15px 0px #000000;
+  box-shadow: 0px 0px 15px 0px rgba(0, 0, 0, 0.5);
+
+  cursor: pointer;
+
+  transition: 0.4s ease all;
+
+  &:active {
+  }
 `;
 
 const CardHeader = styled.div`
@@ -47,6 +70,7 @@ const CardHeader = styled.div`
 
 const CardLyrics = styled.div`
   width: 100%;
+  min-width: 100%;
   height: ${lyricsBoxSizeToContent * (cardSize - 2 * paddingToCard) + "px"};
   display: flex;
   flex-direction: column;
@@ -80,7 +104,7 @@ const HeaderTitle = styled.h4`
   font-family: "Montserrat", sans-serif;
   -webkit-font-smoothing: 3em;
   font-smoothing: 3em;
-  color: white;
+  color: ${(props) => props.color};
   font-weight: 500;
   font-size: 0.9em;
   padding-left: 15px;
@@ -88,7 +112,7 @@ const HeaderTitle = styled.h4`
 
 const HeaderSubtitle = styled.h5`
   font-family: "Montserrat", sans-serif;
-  color: white;
+  color: ${(props) => props.color};
   font-weight: 200;
   font-size: 0.7em;
   padding-left: 15px;
@@ -97,54 +121,147 @@ const HeaderSubtitle = styled.h5`
 const Lyrics = styled.h2`
   font-family: "Montserrat", sans-serif;
   -webkit-font-smoothing: 5em;
-  font-smoothing: 5em;
+  font-smooth: 5em;
 
-  color: white;
-  font-weight: 500;
+  color: ${(props) => props.color};
+  font-weight: 600;
   font-size: 1.1em;
 `;
 
-const LogoText = styled.h2`
+const UpDownButtons = styled("button")`
+  cursor: pointer;
+  display: inline-block;
+
+  position: relative;
+
+  width: fit-content;
+  height: 3.5%;
+  padding: 0px 4px;
+
+  border-radius: 5px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: ${(props) => props.color};
+
+  color: ${(props) => props.color};
+
   font-family: "Montserrat", sans-serif;
-  color: white;
-  font-weight: 550;
-  font-size: 0.75em;
+  font-size: 14px;
+
+  transition: ease all 0.3s;
+
+  &:hover {
+    border-color: ${(props) => props.hover};
+    color: ${(props) => props.hover};
+  }
 `;
 
-const LyricsCard = ({
-  songname,
-  artist,
-  firstline,
-  secondline,
-  thirdline,
-  fourthline,
-  color,
-}) => {
+const LyricsCard = ({ data }) => {
+  var [selected, setSelected] = useState(0);
+  var [lyric, setLyric] = useState(0);
+  var [song, setSong] = useState(0);
+
+  const songname = data[song].title;
+  const artist = data[song].artist;
+  const firstline = data[song].lyrics[lyric];
+  const secondline = data[song].lyrics[lyric + 1];
+  const thirdline = data[song].lyrics[lyric + 2];
+  const fourthline = data[song].lyrics[lyric + 3];
+  const theme = data[song].theme;
+  const fullLyrics = data[song].lyrics;
+  const cover = data[song].src;
+
+  const handleTheme = (theme) => {
+    let dimThemes = theme.length;
+    if (selected === dimThemes - 1) return setSelected(0);
+    return setSelected(selected + 1);
+  };
+
+  const handleChangeLyric = (e) => {
+    let dimLyrics = fullLyrics.length;
+
+    if (e === "fwd") {
+      if (lyric > dimLyrics - 4) return setLyric(0);
+      return setLyric(lyric + 1);
+    } else {
+      if (lyric === 0) return setLyric(dimLyrics - 4);
+      return setLyric(lyric - 1);
+    }
+  };
+
+  const handleChangeSong = () => {
+    let dimSongs = data.length;
+    setLyric(0);
+    if (song === dimSongs - 1) return setSong(0);
+    return setSong(song + 1);
+  };
+
   return (
-    <Wrapper primaryColor={color}>
-      <Card primaryColor={color}>
-        <CardHeader>
-          <HeaderImage src={cover}></HeaderImage>
-          <HeaderText>
-            <HeaderTitle>{songname}</HeaderTitle>
-            <HeaderSubtitle>{artist}</HeaderSubtitle>
-          </HeaderText>
-        </CardHeader>
-        <CardLyrics>
-          <Lyrics>{firstline}</Lyrics>
-          <Lyrics>{secondline}</Lyrics>
-          <Lyrics>{thirdline}</Lyrics>
-          <Lyrics>{fourthline}</Lyrics>
-        </CardLyrics>
-        <CardLogo>
-          <Spotify 
-          height="70%" 
-          width="10%" 
-          color="#FFF"/>
-          <LogoText>Spotify</LogoText>
-        </CardLogo>
-      </Card>
-    </Wrapper>
+    <>
+      <Wrapper color={theme[selected].primaryColor}>
+        <Card
+          color={theme[selected].primaryColor}
+          onClick={() => handleTheme(theme)}
+        >
+          <CardHeader>
+            <HeaderImage
+              style={{ height: "100%" }}
+              src={
+                songname === "Amianto"
+                  ? Amianto
+                  : songname === "Todo Carinho"
+                  ? TodoCarinho
+                  : CafeAmargo
+              }
+            ></HeaderImage>
+            <HeaderText>
+              <HeaderTitle color={theme[selected].secondaryColor}>
+                {songname}
+              </HeaderTitle>
+              <HeaderSubtitle color={theme[selected].secondaryColor}>
+                {artist}
+              </HeaderSubtitle>
+            </HeaderText>
+          </CardHeader>
+          <CardLyrics>
+            <Lyrics color={theme[selected].secondaryColor}>{firstline}</Lyrics>
+            <Lyrics color={theme[selected].secondaryColor}>{secondline}</Lyrics>
+            <Lyrics color={theme[selected].secondaryColor}>{thirdline}</Lyrics>
+            <Lyrics color={theme[selected].secondaryColor}>{fourthline}</Lyrics>
+          </CardLyrics>
+          <CardLogo>
+            <Spotify
+              height="100%"
+              width="25%"
+              fill={theme[selected].secondaryColor}
+            />
+          </CardLogo>
+        </Card>
+        <ButtonWrapper>
+          <UpDownButtons
+            onClick={() => handleChangeLyric("bwd")}
+            color={theme[selected].secondaryColor}
+            hover={theme[selected].secondaryColor + "88"}
+          >
+            Voltar letra
+          </UpDownButtons>
+          <UpDownButtons
+            onClick={() => handleChangeSong()}
+            color={theme[selected].secondaryColor}
+            hover={theme[selected].secondaryColor + "88"}
+          >
+            Alterar música
+          </UpDownButtons>
+          <UpDownButtons
+            onClick={() => handleChangeLyric("fwd")}
+            color={theme[selected].secondaryColor}
+            hover={theme[selected].secondaryColor + "88"}
+          >
+            Avançar letra
+          </UpDownButtons>
+        </ButtonWrapper>
+      </Wrapper>
+    </>
   );
 };
 
